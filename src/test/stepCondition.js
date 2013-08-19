c5580e8c3a688d443f65f371f037cd00aad4ea53
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-08-19 19:10:39 -0400

    checked on value

--- src/test/stepCondition.js
+++ src/test/stepCondition.js
@@ -15,3 +15,16 @@ gt.test('2x2 with max path length 2', function () {
   gt.equal(paths[0], 'ac');
   gt.equal(paths[1], 'ab');
 });
+
+gt.test('2x2 without letter b', function () {
+  var aaab = [['a', 'a'], ['a', 'b']];
+  var paths = pf(aaab, 0, 0, {
+    simple: true,
+    stepWhile: function (path, x, y, grid) {
+      return grid[x][y] !== 'b';
+    }
+  });
+  gt.equal(paths.length, 2, 'only 2 ways');
+  gt.equal(paths[0], 'aa');
+  gt.equal(paths[1], 'aa');
+});
